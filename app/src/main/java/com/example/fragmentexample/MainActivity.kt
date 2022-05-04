dd0c44c30737cd6c6b9a75b02ebb1f1f3e6b2168
Riska Ardila <riskaardila35@gmail.com>
2022-05-04 22:55:04 +0700

    Percobaan Menggunakan Fragment Example

--- app/src/main/java/com/example/fragmentexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -5,15 +5,22 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragBookDescription: BookDescription
+    lateinit var fragBookTitle: BookTitle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragBookTitle = BookTitle()
+        fragBookDescription = BookDescription()
+        val fragTransaction = supportFragmentManager.beginTransaction()
+        fragTransaction.add(R.id.fragtop, fragBookTitle)
+        fragTransaction.add(R.id.fragbottom, fragBookDescription)
+        fragTransaction.commit()
     }
 
-    override fun onBookChanged(index:Int){
-        val frag = fragmentbookdescription
-        if (frag is BookDescription) {
-            frag.changeDescription(index)
-        }
+    override fun onBookChanged(index:Int) {
+        fragBookDescription.changeDescription(index)
     }
 }
